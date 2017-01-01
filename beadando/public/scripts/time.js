d00302f94f69cfdba25f87249ad14891a0091cc6
Dudás Orsolya <orsi1293@gmail.com>
2017-01-01 20:11:54 +0100

    time

--- beadando/public/scripts/time.js
+++ beadando/public/scripts/time.js
@@ -7,7 +7,7 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('time').innerHTML =
-    h + ":" + m + ":" + s;
+    "IDŐ: " + h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
